e04538c33039c26a073b49b6cf2a79e66e72cd2d
pedrok <pedrohlima20@gmail.com>
2019-09-20 18:57:03 -0300

    novos métodos acessores com validações

--- src/Pessoa.cpp
+++ src/Pessoa.cpp
@@ -5,15 +5,17 @@
 using namespace std;
 
 pessoa :: pessoa(){
-    cout << "corno construido"<< endl;
+    cout << "Pessoa construido"<< endl;
 }
 pessoa :: ~pessoa(){
-    cout << "corno destruido"<< endl;
+    cout << "Pessoa destruido"<< endl;
 }
 
 void pessoa :: setnome(){
     string x;
     int z;
+    cout << "Digite o nome do cliente:"<<endl;
+
     do{
     z=0;
     getline(cin, x);
@@ -30,6 +32,56 @@ void pessoa :: setnome(){
     this->nome=x;  
 }
 
-void pessoa :: imprimenome(){
-    cout << nome << endl;
+void pessoa :: setidade(){
+    int x, y;
+    cout << "Digite a idade do cliente:"<<endl;
+    do{
+    y=0;
+    cin >> x;
+    if(x<0 || x>100 || cin.fail()){
+        cout <<"Idade invalida. Digite novamente:"<< endl;
+        y=1;
+    }
+    cin.clear();
+    cin.ignore(32767,'\n');
+    }while(y!=0);
+   
+   this-> idade = to_string(x);
+
+}
+void pessoa :: setcpf(){
+    string x;
+    int y; 
+    cout << "Digite o CPF do cliente:"<<endl;
+    do{
+    y=0;
+    getline(cin, x);
+    for(int i=0;i<x.size();i++){
+    if(!isdigit(x[i]) || isspace(x[i]) || x.size()!=11){
+        y=1;
+        }
+    }
+    if(y==1){
+        cout <<"Cpf invalido. Digite novamente:"<< endl;
+    }
+    
+    }while(y!=0);
+   
+   this-> cpf = x;
+
+}
+string pessoa::getcpf(){
+    return this->cpf;
+}
+string pessoa::getidade(){
+    return this->idade;
+}
+string pessoa::getnome(){
+    return this->nome;
+}
+
+void pessoa :: imprimedados(){
+    cout <<"Nome:"<< getnome() << endl;
+    cout <<"Idade:"<< getidade() << endl;
+    cout << "CPF:"<< getcpf() << endl;
 }
