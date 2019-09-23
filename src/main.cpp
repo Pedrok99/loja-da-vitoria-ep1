35f0c881b654c4a52002e594a196d95aabc3f3c5
pedrok <pedrohlima20@gmail.com>
2019-09-22 22:09:46 -0300

    adição de um menu inicial

--- src/main.cpp
+++ src/main.cpp
@@ -5,17 +5,57 @@
 using namespace std;
 
 int main(){
+  int opc, y;
+  cliente c;
+system("cls");
 
-    cliente c;
+do{
+  cout << "========================================="<< endl;
+  cout << "=          LOJINHU DU PEDRINHU          ="<< endl;
+  cout << "========================================="<< endl;
 
-    c.setnome();
-    c.setidade();
-    c.setcpf();
-    c.isSocio();
-    c.saveclient(&c);
-    c.imprimedados();
-    c.showSocio();
-    c.find();
+  cout <<"\tMenu" << endl;
+  cout <<"1 - Cadastramento de Clientes"<< endl;
+  cout <<"2 - Modo Venda"<< endl;
+  cout <<"3 - Modo Estoque"<< endl;
+  cout <<"4 - Modo Recomendacao"<< endl;
+  cout <<"0 - Sair" << endl;
 
-    return 0;
+  do{
+  y=0;
+  cin >> opc;
+  if(cin.fail()){
+  cin.clear();
+  cin.ignore(10000, '\n');
+  y=1;
+  }
+  }while(y==1);
+
+  system("cls");
+
+  switch (opc)
+  {
+  case 1:
+  cin.clear();    
+  cin.ignore(10000, '\n');
+  c.setnome();
+  c.setidade();
+  c.setcpf();
+  c.isSocio();
+  c.saveclient(&c);
+  cout << "Cliente cadastrado e salvo com sucesso !!" << endl;
+  break;
+
+  case 2:
+    
+  break;
+
+
+
+  default:
+  break;
+  };
+}while(opc!=0);
+   
+return 0;
 }
