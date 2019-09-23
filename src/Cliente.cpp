6cf12308c2d589020306596e5fb9a9b70672900b
pedrok <pedrohlima20@gmail.com>
2019-09-22 22:10:25 -0300

    adiçao de metodo para procura de clientes

--- src/Cliente.cpp
+++ src/Cliente.cpp
@@ -3,42 +3,40 @@
 #include <fstream>
 #include "Cliente.hpp"
 #include <vector>
-#include <fstream>
 
 using namespace std;
 
 
 cliente::cliente(){
-    cout << "cliente construido"<<endl;
+   // cout << "cliente construido"<<endl;
 }
 
 cliente::~cliente(){
-    cout << "cliente destruido"<<endl;
+   // cout << "cliente destruido"<<endl;
 }
 
 void cliente::isSocio() {
 
-    char x, z;
-    cout << "O cliente eh socio?"<< endl;
+    string x, z;
+    cout << "O cliente eh socio?[s/n]"<< endl;
     do{
         cin >> x;
-        z ='a';
-        if(cin.fail() ||  (x != 's' && x!='n')){
+        z ="a";
+        if(cin.fail() ||  (x != "s" && x!="n")){
             cin.clear();
             cin.ignore(32767,'\n');
             cout << "Entrada invalida. Digite novamente:"<< endl;
-            z='b';
+            z="b";
         }
-        
-    }while(z!='a');
+    }while(z!="a");
     this -> socio = x;
 }
-void cliente :: isSocio(char x){
+void cliente :: isSocio(string x){
     this->socio=x;
 }
 
 void cliente :: showSocio(){
-    if(this->socio == 's'){
+    if(this->socio == "s"){
         cout << "Cliente eh socio"<< endl;
     }else{
         cout << "Cliente nao eh socio"<<endl;
@@ -53,6 +51,48 @@ void cliente :: saveclient(cliente *p){
     arq << p->socio << endl;
     arq.close();
 }
+
+
+
 void cliente :: find(){
-    cout << "função para encontrar clientes" << endl;
+    string x, inf;
+    int i=0, pos=0, y; 
+    fstream arquivo;
+    vector<string> v;
+    cliente temp;
+    cout << "Digite o cpf do cliente a ser encontrado:" << endl;
+     do{
+        y=0;
+        getline(cin, x);
+        for(int c=0;c<x.size();c++){
+            if(!isdigit(x[c]) || isspace(x[c]) || x.size()!=11){
+                y=1;
+            }
+        }
+        if(y==1){
+        cout <<"Cpf invalido. Digite novamente:"<< endl;
+    }
+    
+    }while(y!=0);
+
+    arquivo.open("Clientes.txt", ios::in);
+    i=0;
+
+    while (getline(arquivo, inf))
+    {
+        v.push_back(inf);
+        if(v[i]==x){
+            cout << "Cliente encontrado" << endl;
+            pos=i-2;
+            y=1;
+        }
+        i++;
+    }
+    if(y==1){
+        cout << "A posicao do cliente é" << pos << endl;
+    }else{
+        cout << "cliente nao encontrado !!"<< endl;
+    }
+    arquivo.close();
+
 }
