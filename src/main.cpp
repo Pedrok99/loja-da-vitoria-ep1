ee7c15c7be65bd978fe3d4c88b92bfda30a40034
pedrok <pedrohlima20@gmail.com>
2019-09-24 23:17:18 -0300

    Criação do modo venda e estoque

--- src/main.cpp
+++ src/main.cpp
@@ -6,70 +6,130 @@
 #include <vector>
 using namespace std;
 
-int main(){
+void limpa()
+{
+  cout << "\n\n\n\n\n\n"
+       << endl;
+}
+
+int main()
+{
   int opc, checker;
   string aux;
   cliente c;
   produto p;
-  vector<string> vet; 
- 
-system("cls");
+  vector<string> vet, vaux;
 
-do{
-  cout << "========================================="<< endl;
-  cout << "=          LOJINHU DU PEDRINHU          ="<< endl;
-  cout << "========================================="<< endl;
-
-  cout <<"\t\t   Menu\n" << endl;
-  cout <<"1 - Modo Venda"<< endl;
-  cout <<"2 - Modo Estoque"<< endl;
-  cout <<"3 - Modo Recomendacao"<< endl;
-  cout <<"0 - Sair" << endl;
-
-  do{
-  checker=0;
-  cin >> opc;
-  if(cin.fail()){
-  cin.clear();
-  cin.ignore(10000, '\n');
-  checker=1;
-  }
-  }while(checker==1);
-
-  system("cls");
-
-  switch (opc)
+  do
   {
-  case 1:
-    cin.clear();
-    cin.ignore(10000,'\n');/*
-    c=c.find();
-    aux=c.getnome();
-   if(c.getnome()=="0"){
-      cout << "Por favor cadastre-o." << endl;
-      c.setnome();
-      c.setidade();
-      c.setcpf();
-      c.isSocio();
-    }else{
-    }
-    c.imprimedados();
-    c.showSocio();
-    c.saveclient(&c);*/
-    vet=p.getlista();
-    
-  break;
+    cout << "=========================================" << endl;
+    cout << "=          LOJINHU DU PEDRINHU          =" << endl;
+    cout << "=========================================" << endl;
 
-  case 2:
-    
-  break;
+    cout << "\t\t   Menu\n"
+         << endl;
+    cout << "1 - Modo Venda" << endl;
+    cout << "2 - Modo Estoque" << endl;
+    cout << "3 - Modo Recomendacao" << endl;
+    cout << "0 - Sair" << endl;
 
+    do
+    {
+      checker = 0;
+      cin >> opc;
+      if (cin.fail())
+      {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        checker = 1;
+      }
+    } while (checker == 1);
 
+    limpa();
 
-  default:
-  break;
-  };
-}while(opc!=0);
-   
-return 0;
+    switch (opc)
+    {
+    case 1:
+
+      cin.clear();
+      cin.ignore(10000, '\n');
+      c = c.find();
+      if (c.getnome() == "0")
+      {
+        cout << "Por favor cadastre o cliente." << endl;
+        c.setnome();
+        c.setidade();
+        c.setcpf();
+        c.isSocio();
+        c.saveclient(&c);
+      }
+      else
+      {
+      }
+      cout << c.getnome() << endl;
+      // c.imprimedados();
+      // c.showSocio();
+      cout << "Pressione qualquer tecla para ir ao catalogo de produtos..." << endl;
+      getchar();
+      vet = p.getlista();
+      p.carrinho(vet, c.getSocio());
+      cout << "Insira qualquer coisa para retornar ao menu :" << endl;
+
+      break;
+
+    case 2:
+      cout << "0 - Novo Produto." << endl;
+      cout << "1 - Produto existente." << endl;
+      do
+      {
+        checker = 0;
+        cin >> opc;
+        if (cin.fail() || opc >= 2)
+        {
+          cout << "Opcao Invalida" << endl;
+          cin.clear();
+          cin.ignore(10000, '\n');
+          checker = 1;
+        }
+      } while (checker == 1);
+
+      if (opc == 0)
+      {
+        cin.clear();
+        cin.ignore(10000, '\n');
+        p.setnome();
+        p.setcategoria();
+        p.setquantidade();
+        p.setpreco();
+        p.saveproduct(&p);
+        cout << "Produto adicionado !!" << endl;
+      }
+      else
+      {
+        vaux = p.getlista();
+        do
+        {
+          cout << "Digite o codigo do produto que sera atualizado" << endl;
+          checker = 0;
+          cin >> opc;
+          cout << vaux.size() << endl;
+          if (cin.fail() || (opc * 4) >= vaux.size() || opc < 0)
+          {
+            cout << "Opcao Invalida" << endl;
+            cin.clear();
+            cin.ignore(10000, '\n');
+          }
+        } while (checker == 1);
+        p.attproduto(p.getlista(), opc);
+      
+      }
+      opc=1;
+      break;
+
+    default:
+      break;
+    };
+  } while (opc != 0);
+
+  return 0;
 }
