7f98e53e8266518221909952d870ab0c17227d5c
pedrok <pedrohlima20@gmail.com>
2019-09-24 23:16:13 -0300

    Correçoes em alguns metodos acessores

--- src/Pessoa.cpp
+++ src/Pessoa.cpp
@@ -1,99 +1,136 @@
 #include <iostream>
 #include <string>
 #include "Pessoa.hpp"
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-pessoa :: pessoa(){
-   // cout << "Pessoa construida"<< endl;
+pessoa ::pessoa()
+{
+    // cout << "Pessoa construida"<< endl;
 }
-pessoa :: ~pessoa(){
+pessoa ::~pessoa()
+{
     //cout << "Pessoa destruida"<< endl;
 }
 
-void pessoa :: setnome(){
+void pessoa ::setnome()
+{
     string x;
     int z;
-    //cin.clear();
-    //cin.ignore(10000, '\n');
-    cout << "Digite o nome do cliente:"<<endl;
-    do{
-    z=0;
-    getline(cin, x);
-    for(int i=0;i<x.size();i++){
-        if(isalpha(x[i]) || isspace(x[i])) {}
-        else{
-            cout << "O nome do cliente nao deve possuir caracteres especiais ou numeros!!"<<endl;
-            cout << "Insira-o novamente:"<< endl;
-            z=1;
-            break;
-        }
-     }
-    }while(z!=0);
-    this->nome=x;  
-}
-void pessoa :: setnome(string nome){
-    this->nome = nome;
-}
-void pessoa :: setidade(){
-    int x, y;
-    cout << "Digite a idade do cliente:"<<endl;
-    do{
-    y=0;
-    cin >> x;
-    if(x<0 || x>100 || cin.fail()){
-        cout <<"Idade invalida. Digite novamente:"<< endl;
-        y=1;
-    }
-    cin.clear();
-    cin.ignore(32767,'\n');
-    }while(y!=0);
-   
-   this-> idade = to_string(x);
-
-}
-void pessoa :: setidade(string idade){
-    this->idade = idade;
-}
-void pessoa :: setcpf(){
-    string x;
-    int y; 
-    cout << "Digite o CPF do cliente:"<<endl;
-    do{
-     y=0;
+    cout << "Digite o nome do cliente:" << endl;
+    do
+    {
+        z = 0;
         getline(cin, x);
-        for(int i=0;i<x.size();i++){
-            if(!isdigit(x[i]) || isspace(x[i]) || x.size()!=11){
-            y=1;
+        for (int i = 0; i < x.size(); i++)
+        {
+            if (isalpha(x[i]) || isspace(x[i]))
+            {
+            }
+            else
+            {
+                cout << "O nome do cliente nao deve possuir caracteres especiais ou numeros!!" << endl;
+                cout << "Insira-o novamente:" << endl;
+                z = 1;
+                break;
             }
         }
-    if(y==1){
-        cout <<"Cpf invalido. Digite novamente:"<< endl;
-    }
-    
-    }while(y!=0);
-   
-   this-> cpf = x;
+        
+    } while (z != 0);
+    this->nome = x;
+}
+void pessoa ::setnome(string nome)
+{
+    this->nome = nome;
+}
+void pessoa ::setidade()
+{
+    int x, y;
+    cout << "Digite a idade do cliente:" << endl;
+    do
+    {
+        y = 0;
+        cin >> x;
+        if (x < 0 || x > 100 || cin.fail())
+        {
+            cout << "Idade invalida. Digite novamente:" << endl;
+            y = 1;
+        }
+        cin.clear();
+        cin.ignore(32767, '\n');
+    } while (y != 0);
 
+    this->idade = to_string(x);
+}
+void pessoa ::setidade(string idade)
+{
+    this->idade = idade;
+}
+void pessoa ::setcpf()
+{
+    string x, aux;
+    int y, i, cont;
+    vector<string> cpfs;
+    fstream arq;
+    cout << "Digite o CPF do cliente:" << endl;
+    do
+    {
+        y = 0;
+        getline(cin, x);
+        for (int i = 0; i < x.size(); i++)
+        {
+            if (!isdigit(x[i]) || isspace(x[i]) || x.size() != 11)
+            {
+                y = 1;
+            }
+        }
+        if (y == 1)
+        {
+            cout << "Cpf invalido. Digite novamente:" << endl;
+        }
+        if (y != 1)
+        {
+            arq.open("Clientes.txt", ios::in);
+            while (getline(arq, aux))
+            {
+                if (aux == x)
+                {
+                    cout << "Cpf ja cadastrado. Digite novamente:" << endl;
+                    y = 1;
+                }
+            }
+            arq.close();
+        }
+
+    } while (y != 0);
+
+    this->cpf = x;
 }
 
-void pessoa :: setcpf(string cpf){
-    this->cpf= cpf;
+void pessoa ::setcpf(string cpf)
+{
+    this->cpf = cpf;
 }
 
-string pessoa::getcpf(){
+string pessoa::getcpf()
+{
     return this->cpf;
 }
 
-string pessoa::getidade(){
+string pessoa::getidade()
+{
     return this->idade;
 }
-string pessoa::getnome(){
+string pessoa::getnome()
+{
     return this->nome;
 }
 
-void pessoa :: imprimedados(){
-    cout <<"Nome:"<< getnome() << endl;
-    cout <<"Idade:"<< getidade() << endl;
-    cout << "CPF:"<< getcpf() << endl;
+void pessoa ::imprimedados()
+{
+    cout << "Nome:" << getnome() << endl;
+    cout << "Idade:" << getidade() << endl;
+    cout << "CPF:" << getcpf() << endl;
 }
