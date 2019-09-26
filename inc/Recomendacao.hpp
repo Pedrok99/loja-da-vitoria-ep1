c50213bbc498d52d2c43c86ea808ae2a3191c9dd
pedrok <pedrohlima20@gmail.com>
2019-09-26 13:38:03 -0300

    criação de prototipos de metodos para classe de recomendaçoes

---
+++ inc/Recomendacao.hpp
@@ -0,0 +1,23 @@
+#ifndef recomendacao_hpp
+#define recomendacao_hpp
+#include <iostream>
+#include <string>
+#include "Produto.hpp"
+
+class recomendacao : public produto{
+
+
+
+public:
+    recomendacao();
+    ~recomendacao();
+    vector<string> separacategoria();
+    vector<string> separacategoria(vector<string> v);
+    void historico(vector<string> categorias, string cpf);
+    void salvacategoria(vector<string> novo);
+    vector<string> gethistorico();
+    void recomenda();
+    
+
+};
+#endif
